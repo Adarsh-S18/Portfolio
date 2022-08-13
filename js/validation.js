e11f6fed56a1335e3df6cb4ead49518e8c4879b0
Adarsh-S18 <adarshspillai2001@gmail.com>
2022-08-13 10:37:32 +0530

    first commit

--- js/validation.js
+++ js/validation.js
@@ -1,68 +1,68 @@
 $(document).ready(function () {
-     
-    jQuery.validator.addMethod(
-      "lettersonly",
-      function (value, element) {
-        return this.optional(element) || /^[a-z A-Z]+$/.test(value);
+
+  jQuery.validator.addMethod(
+    "lettersonly",
+    function (value, element) {
+      return this.optional(element) || /^[a-z A-Z]+$/.test(value);
+    },
+    "Letters only please"
+  );
+  jQuery.validator.addMethod(
+    "minlength5",
+    function (value, element) {
+      return this.optional(element) || (value.trim().length >= 5);
+    },
+    "Minimum 5 characters without space"
+  );
+  $(".contact-forms").validate({
+    rules: {
+      name: {
+        required: true,
+        minlength5: true,
+        lettersonly: true,
+        minlength: 4,
       },
-      "Letters only please"
-    );
-    jQuery.validator.addMethod(
-      "minlength5",
-      function (value, element) {
-        return this.optional(element) || (value.trim().length >= 5);
+      email: {
+        required: true,
+        email: true,
       },
-      "Minimum 5 characters without space"
-    );
-    $(".contact-forms").validate({
-      rules: {
-          name: {
-            required: true,
-          minlength5: true,
-          lettersonly: true,
-          minlength: 4,
-        },
-        email: {
-          required: true,
-          email: true,
-        },
-        message: {
-            required: true,
-          minlength5: true,
-          minlength: 10,
-          maxlength: 200,
-        },
+      message: {
+        required: true,
+        minlength5: true,
+        minlength: 10,
+        maxlength: 200,
       },
-      messages: {
-        name: {
-          minlength: "Please Enter Your Full Name",
-        },
-        email: {
-          email: "Please enter a valid Email id",
-        },
-        message: {
-          minlength: "Its too short! minimum 10 characters",
-          maxlength: "Oh no! it's too large",
-        },
+    },
+    messages: {
+      name: {
+        minlength: "Please Enter Your Full Name",
       },
-      submitHandler: function (form) {
-        console.log("True");
-        console.log("in function submit");
-        submit();
+      email: {
+        email: "Please enter a valid Email id",
       },
-    });
+      message: {
+        minlength: "Its too short! minimum 10 characters",
+        maxlength: "Oh no! it's too large",
+      },
+    },
+    submitHandler: function (form) {
+      console.log("True");
+      console.log("in function submit");
+      submit();
+    },
   });
-  function submit() {
-    $.ajax({
-      url: "https://script.google.com/macros/s/AKfycbwtPf3FVc9qjj0FA9ddD21EKvspv68I9kRVbAPXs8H34qvcfhaHtnvLeeEwgMeISgZb/exec",
-      data: $(".contact-forms").serialize(),
-      method: "POST",
-      success: function (response) {
-        alert("Form submitted successfully");
-        window.location.reload();
-      },
-      error: function (err) {
-        alert("Something Error");
-      },
-    });
-  }
+});
+function submit() {
+  $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbzqoaOeLS1iqoNxXSQRCPxOvapQpLgyNOgnU4dStNjDHg6Vl_XcL-eawplmcpXOPMkJ/exec",
+    data: $(".contact-forms").serialize(),
+    method: "POST",
+    success: function (response) {
+      alert("Form submitted successfully");
+      window.location.reload();
+    },
+    error: function (err) {
+      alert("Something Error");
+    },
+  });
+}
